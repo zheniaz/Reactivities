0736e86757f647101810c45a4cd74e6ed10ebdc6
zheniazgurovets <zhenia.zgurovets@gmail.com>
2025-07-12 19:18:23 -0700

    DB minor fix

---
+++ Persistence/Migrations/20250713021349_Fix1Added.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix1Added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ActivityAttendees",
+                table: "ActivityAttendees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ActivityAttendees_ActivityId",
+                table: "ActivityAttendees");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ActivityAttendees",
+                table: "ActivityAttendees",
+                columns: new[] { "ActivityId", "UserId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityAttendees_UserId",
+                table: "ActivityAttendees",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ActivityAttendees",
+                table: "ActivityAttendees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ActivityAttendees_UserId",
+                table: "ActivityAttendees");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ActivityAttendees",
+                table: "ActivityAttendees",
+                columns: new[] { "UserId", "ActivityId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityAttendees_ActivityId",
+                table: "ActivityAttendees",
+                column: "ActivityId");
+        }
+    }
+}
